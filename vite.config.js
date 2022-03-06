d7485308492c41febe39a44825d25d8aab273fdf
moisesBenites <moisesbenites3@gmail.com>
2022-03-06 03:27:12 -0600

    fix bug en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://moisesbenites.github.io/',
+  base: 'https://moisesbenites.github.io/gh-react/',
   plugins: [react()],
 });
